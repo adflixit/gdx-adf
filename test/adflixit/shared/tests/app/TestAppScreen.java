399f313fbfd4172ece26e55e5fb29e7d179119e6
Adflixit <adflixit@gmail.com>
2018-09-21 13:48:14 +0300

    Add files via upload

--- test/adflixit/shared/tests/app/TestAppScreen.java
+++ test/adflixit/shared/tests/app/TestAppScreen.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Adflixit
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package adflixit.shared.tests.app;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
@@ -5,15 +21,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import adflixit.shared.BaseScreen;
 
 public class TestAppScreen extends BaseScreen<TestApp> {
-
-	public TestAppScreen(TestApp game) {
-		super(game);
+	public TestAppScreen() {
+		super();
+		game = new TestApp();
 	}
 
 	@Override public void goBack() {
 	}
 
-	public static void launch(BaseScreen<?> screen) {
-		new LwjglApplication(new TestApp(screen), "Test App", 360, 640);
+	public static void launch(TestAppScreen screen) {
+		new LwjglApplication(screen.game, "Test App", 360, 640);
 	}
 }
